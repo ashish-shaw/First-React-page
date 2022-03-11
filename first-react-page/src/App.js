d2e26c5d59beaf0f48d086c6d7ae058fefd2e06a
ASHISH KUMAR SHAW <91532940+ashish-shaw@users.noreply.github.com>
2022-03-11 22:05:36 +0530

    Add files via upload

---
+++ first-react-page/src/App.js
@@ -0,0 +1,17 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Ashish Shaw Netflix Pick</h1>
+      <p>List of 5 best series</p>
+      <ol>
+        <li>Dark</li>
+        <li>Extra Circular</li>
+        <li>My Holo Love</li>
+        <li>My first 2 Love</li>
+        <li>Mr. Robot</li>
+      </ol>
+    </div>
+  );
+}
